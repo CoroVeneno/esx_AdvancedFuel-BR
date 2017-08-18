69fb3aaaabdc29e7de28812393f65209595109c4
Allan <31086346+TakumiBR@users.noreply.github.com>
2017-08-17 18:36:55 -0700

    Brazilian Portuguese translation

---
+++ config_c.lua
@@ -0,0 +1,61 @@
+lang = "br"
+-- lang = "en"
+-- lang = "fr"
+
+settings = {}
+
+settings["br"] = {
+	openMenu = "Pressione ~g~E~w~ para abrir o menu.",
+	electricError = "~r~Você tem um veículo elétrico.",
+	fuelError = "~r~Você não está em um bom lugar.",
+	buyFuel = "Comprar combustível",
+	liters = "Litros",
+	percent = "Por cento",
+	confirm = "Confirmar",
+	fuelStation = "Posto de Gasolina",
+	boatFuelStation = "Posto de Gasolina | Barco",
+	avionFuelStation = "Posto de Gasolina | Avião ",
+	heliFuelStation = "Posto de Gasolina | Helicoptero",
+	price = "preço"
+
+settings["en"] = {
+	openMenu = "Press ~g~E~w~ to open the menu.",
+	electricError = "~r~You have an electric vehicle.",
+	fuelError = "~r~You're not in the good place.",
+	buyFuel = "buy fuel",
+	liters = "liters",
+	percent = "percent",
+	confirm = "Confirm",
+	fuelStation = "Fuel station",
+	boatFuelStation = "Fuel station | Boat",
+	avionFuelStation = "Fuel station | Plane ",
+	heliFuelStation = "Fuel station | Helicopter",
+	price = "price"
+}
+
+settings["fr"] = {
+	openMenu = "Appuyez sur ~g~E~w~ pour ouvrir le menu.",
+	electricError = "~r~Vous avez une voiture électrique.",
+	fuelError = "~r~Vous n'êtes pas au bon endroit.",
+	buyFuel = "acheter de l'essence",
+	liters = "litres",
+	percent = "pourcent",
+	confirm = "Valider",
+	fuelStation = "Station essence",
+	boatFuelStation = "Station d'essence | Bateau",
+	avionFuelStation = "Station d'essence | Avions",
+	heliFuelStation = "Station d'essence | Hélicoptères",
+	price = "prix"
+}
+
+
+
+hud_form = 1 -- 1 : Vertical | 2 = Horizontal
+hud_x = 0.175 
+hud_y = 0.885
+
+text_x = 0.2575
+text_y = 0.975
+
+
+electricityPrice = 1 -- NOT RANDOMED !!!
